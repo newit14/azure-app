a237e200d886e6de5f1f730d7825b2b5d4c0449e
Sigi <sigi.newit@gmail.com>
2023-10-29 14:08:17 +0100

    azure backend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Hello} from "../model/hello";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'azure-app';
+  messageDto: Hello = {message: 'load from backend...'};
+
+
+  constructor(private httpClient: HttpClient ) {
+  }
+
+  ngOnInit(): void {
+    this.httpClient.get<Hello>("https://french-app.jollywater-bf4065db.westeurope.azurecontainerapps.io")
+      .subscribe(r =>
+    this.messageDto = r)
+  }
+
 }
+//https://french-app.jollywater-bf4065db.westeurope.azurecontainerapps.io
